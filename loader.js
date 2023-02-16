39c4b2dd4a4b545cd77d6d5a430e0da985c10fb2
Dylan LEFEBVRE <dylan.lefebvre76@live.com>
2023-02-16 15:47:42 +0100

    correction loader + media query mobile / tablette

--- loader.js
+++
@@ -1,8 +0,0 @@
-const progressBar = document.querySelector(".progress-bar");
-
-progressBar.addEventListener("animationend", () => {
-  if (progressBar.style.width === "100%") {
-    window.location.href =
-      "https://dylanl-dev.github.io/P2openclassrooms.github.io/";
-  }
-});
